0909c8fe66991d0cc196890a8193cb0191d6e203
marketaandrsova <130682732+marketaandrsova@users.noreply.github.com>
2024-11-10 15:44:36 +0100

    Update heyfomo.py

--- heyfomo.py
+++ heyfomo.py
@@ -5,7 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome()
 
 driver.get('https://heyfomo.cz/autori')
 
