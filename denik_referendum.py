b33bce5881e7947e8a45212a73c95d62a12d098e
marketaandrsova <130682732+marketaandrsova@users.noreply.github.com>
2024-11-10 15:44:23 +0100

    Update denik_referendum.py

--- denik_referendum.py
+++ denik_referendum.py
@@ -5,7 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome()
 
 driver.get('https://denikreferendum.cz/stranka/redakce')
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'drbutton.svelte-1w4oywx.invert.whiteBorder'))).click()
